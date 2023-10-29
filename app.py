87572ba163263bcfc3ddaeced62840afefeebee9
Mitja <mituuuuz@hotmail.com>
2023-10-29 10:04:28 +0200

    Add configurable schedule names

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import os
 
 app = Flask(__name__)
 
-if os.path.exists('config.py'):
+script_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(script_dir, 'config.py')
+
+if os.path.exists(config_path):
+    print('Found')
     app.config.from_object('config.Config')
 else:
+    print('Not found')
     app.config.from_object('config_example.Config')
 
 @app.route('/')
