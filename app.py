54b73aaf8d12e2bdfc391f23ecf41bcbca325a97
Mitja <mituuuuz@hotmail.com>
2023-10-29 09:46:51 +0200

    Add config files
    Add config files for local and vcs

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
+if os.path.exists('config.py'):
+    app.config.from_object('config.Config')
+else:
+    app.config.from_object('config_example.Config')
 
 @app.route('/')
 def home():
--- config_example.py
+++ config_example.py
@@ -1,2 +1,4 @@
 class Config:
-    
+    DISABLED = 'Disabled'
+    SCHEDULE_1 = 'Schedule 1'
+    SCHEDULE_2 = 'Schedule 2'
