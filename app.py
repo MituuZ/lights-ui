d98dea0b3bee7886cf7f066beffb6c278956a4a8
Mitja <mituuuuz@hotmail.com>
2023-10-29 12:29:11 +0200

    Add logic to populate the data

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 from crontab import CronTab
 import os
 
+def calculate_selected_option(job):
+    hour = str(job.hour)
+    if hour.isdigit() and int(hour) < 5:
+        return app.config['SCHEDULE_1']
+    if hour.isdigit() and int(hour) < 7:
+        return app.config['SCHEDULE_2']
+    if not job.is_enabled:
+        return app.config['DISABLED']
+    return 'No match'
+
 app = Flask(__name__)
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,17 +28,14 @@ cron = CronTab(user=app.config['USER'])
 cron_data = {}
 for job in cron:
     if 'morning-light.sh' in job.command:
-        cron_data[job.comment] = job
-
-items = list(cron_data.items())
-
-for item in items:
-    print(item)
-    print(item[0])
+        cron_data[job.comment] = {
+            'job': job,
+            'selected_option': calculate_selected_option(job)
+        }
 
 @app.route('/')
 def home():
-    return render_template('index.html', items=items)
+    return render_template('index.html', cron_data=cron_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
